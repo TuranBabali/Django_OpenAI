416e0d291e79acd71b46d9a7958bb2894e6fa748
TuranBabali <babalituran7@gmail.com>
2025-04-18 04:50:24 +0200

    test3

--- authorisation/views.py
+++ authorisation/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 def login(request):
     return render(request, 'authorisation/login.html')
 
 def register(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        print('Username submitted was: {}'.format(username))
+
+        return redirect('register')
+
+
     return render(request, 'authorisation/register.html')
